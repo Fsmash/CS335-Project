2636470d595650384745c53d6195ffd6b19982ba
Justin Ramirez <jramirez@cs.csubak.edu>
2015-04-30 13:47:14 -0700

    added gray to health bar

--- UI/UI.h
+++ UI/UI.h
@@ -2,9 +2,9 @@
 #ifndef _UI_H_
 #define _UI_H_
 
-void renderUI(Game *g, int xr, int yr);
+void renderHealth(Game *g, int xr, int yr);
 
-void renderUI(Game *g, int xr, int yr)
+void renderHealth(Game *g, int xr, int yr)
 {
 
 
@@ -16,6 +16,19 @@ void renderUI(Game *g, int xr, int yr)
 	int yres = yr;
 	int spacing = 15;
 
+	for (int i = 0; i < 15; i++) {
+        glPushMatrix();
+		glColor3f(155.0/255.0,169.0/255.0,178.0/255.0);
+        glTranslatef(x+(i*spacing), yres-y, 0.0f);
+        glBegin(GL_QUADS);
+        glVertex2f(-width, -height);
+        glVertex2f(-width, height);
+        glVertex2f(width, height);
+        glVertex2f(width, -height);
+        glEnd();
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glPopMatrix();
+	}
 	for (int i = 0; i < g->player.getHealth(); i++) {
         glPushMatrix();
         glColor3f(255/255.0,0.0,0.0);
@@ -25,10 +38,6 @@ void renderUI(Game *g, int xr, int yr)
         glVertex2f(-width, height);
         glVertex2f(width, height);
         glVertex2f(width, -height);
-        glEnd();
-        glColor3f(1.0f, 0.0f, 0.0f);
-        glBegin(GL_POINTS);
-        glVertex2f(0.0f, 0.0f);
         glEnd();
         glPopMatrix();
 	}
